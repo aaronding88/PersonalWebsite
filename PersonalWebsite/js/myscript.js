affdf7dccc5dd8dd5743fc9e9d36c35c63abaada
aaronding88 <aaronding@aarons-mbp.gateway.pace.com>
2015-03-31 22:34:25 -0700

    NavBar updates
    Major NavBar updates, which include transition, Spyscroller, and more
    consistent banners in the Featured bay.

--- PersonalWebsite/js/myscript.js
+++ PersonalWebsite/js/myscript.js
@@ -1,3 +1,35 @@
-$('.carousel').carousel({
-  interval: false
+$(function () {
+    "use strict";
+    
+    var topoffset = 100; // Variable for the offset of the menu.
+
+    // Activate Scrollspy, targeting Body
+    $('body').scrollspy({
+        target: 'header .navbar',
+        offset: topoffset
+    });
+    
+    // Fire this event when the page is loaded, in case someone links directly to a reference link.
+    var hash = $(this).find('li.active a').attr('href');
+
+    if (hash !== '#main'){
+        $('header nav').addClass('inBody');
+    } else {
+        $('header nav').removeClass('inBody');
+    }
+
+    $('.navbar-fixed-top').on('activate.bs.scrollspy', function() {
+        // Looks for an active "li's" anchor, and stores for the "href" attribute in there.
+        var hash = $(this).find('li.active a').attr('href');
+
+        if (hash !== '#main'){
+            $('header nav').addClass('inBody');
+        } else {
+            $('header nav').removeClass('inBody');
+        }
+    });
+    
+    $('.carousel').carousel({
+      interval: false
+    });
 });
