21d3a152ced239f3eceda323a2fdb50af0116c1a
aaronding88 <aaronding@Aarons-MacBook-Pro.local>
2015-04-02 21:31:21 -0700

    More work on Aesthetics and compatibility
    Added better aesthetics, and much of the design has been optimized for
    better viewing as many resolutions as possible.

--- PersonalWebsite/js/myscript.js
+++ PersonalWebsite/js/myscript.js
@@ -1,8 +1,9 @@
 $(function () {
     "use strict";
     
-    var topoffset = 100; // Variable for the offset of the menu.
-
+    var topoffset = 50; // Variable for the offset of the menu.
+    var slideqty = $('#main .item').length;
+    
     // Activate Scrollspy, targeting Body
     $('body').scrollspy({
         target: 'header .navbar',
@@ -17,7 +18,8 @@ $(function () {
     } else {
         $('header nav').removeClass('inBody');
     }
-
+    
+    // Everytime the navbar activates the scrollspy, perform this function. 
     $('.navbar-fixed-top').on('activate.bs.scrollspy', function() {
         // Looks for an active "li's" anchor, and stores for the "href" attribute in there.
         var hash = $(this).find('li.active a').attr('href');
@@ -29,7 +31,33 @@ $(function () {
         }
     });
     
+    //Automatically generate carousel indicators
+    /* Credit to: Ray Villalobos, Lynda.com */
+    for (var i=0; i < slideqty; i++) {
+    var insertText = '<li data-target="#main" data-slide-to="' + i + '"></li>';
+    $('#main ol').append(insertText);
+  }
+    
+    // Settings for the carousel.
     $('.carousel').carousel({
       interval: false
     });
+    
+    //Use smooth scrolling when clicking on navigation
+    /* Credit to: Ray Villalobos, Lynda.com */
+    $('.navbar a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') === 
+      this.pathname.replace(/^\//,'') && 
+      location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        // Animation section.
+        $('html,body').animate({
+          scrollTop: target.offset().top-topoffset+2
+        }, 500);
+        return false;
+      } //target.length
+    } //click function
+  }); //smooth scrolling
 });
