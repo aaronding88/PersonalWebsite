09b58fd846c506d688d6753ad9a6383a83d7bea9
aaronding88 <aaronding88@gmail.com>
2015-04-07 01:47:38 -0700

    Major Changes to structure and vision
    Numerous changes to the format, deleted much of the projects content.
    Adjusted the y-height of the page to fit the screen.

--- PersonalWebsite/js/myscript.js
+++ PersonalWebsite/js/myscript.js
@@ -3,6 +3,25 @@ $(function () {
     
     var topoffset = 50; // Variable for the offset of the menu.
     var slideqty = $('#main .item').length;
+    var wheight = $(window).height(); // Gets the height of the window.
+    
+    $('.fullheight').css('height', wheight);
+    
+    // replaces IMG inside carousels with a background image
+    // Credit to: Ray Villalobos, Lynda.com
+    
+    $('#main .item img').each(function() {
+        var imgSrc = $(this).attr('src');
+        $(this).parent().css({'background-image': 'url('+imgSrc+')'});
+        $(this).remove();
+    });
+    
+    // Adjust height of .fullheight elements on window resize. Resize activates when it registers a change in size.
+    // Credit to: Ray Villalobos, Lynda.com
+    $(window).resize(function() {
+        wheight = $(window).height(); // Regrabs the window height.
+        $('.fullheight').css('height', wheight); //Applies that window height.
+    });
     
     // Activate Scrollspy, targeting Body
     $('body').scrollspy({
@@ -28,6 +47,18 @@ $(function () {
             $('header nav').addClass('inBody');
         } else {
             $('header nav').removeClass('inBody');
+        }
+    });
+    
+    // When the NavBar hits About Me, add a function to a class.
+    $('.navbar-fixed-top').on('activate.bs.scrollspy', function() {
+        // Looks for an active "li's" anchor, and stores for the "href" attribute in there.
+        var hash = $(this).find('li.active a').attr('href');
+
+        if (hash === '#aboutMe'){
+            $('#about-me-banner').addClass('description-text');
+        } else {
+            $('#about-me-banner').removeClass('description-text');
         }
     });
     
