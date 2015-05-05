6ccca3a61c01845c5f607f11cb003f9086f914ff
aaronding88 <aaronding88@gmail.com>
2015-05-04 23:16:58 -0700

    Heavy CSS cleanup
    Styling conventions added. Attempted to add PHP to the code, but it
    seems somewhat complicated. Did not want to dive too deep into it.

--- PersonalWebsite/form.php
+++ PersonalWebsite/form.php
@@ -5,7 +5,7 @@
         $message = $_POST['message'];
         $human = intval($_POST['human']);
         $from = 'Demo Contact Form'; 
-        $to = 'aaronding88@gmail.com'; 
+        $to = 'example@bootstrapbay.com'; 
         $subject = 'Message from Contact Demo ';
         
         $body = "From: $name\n E-Mail: $email\n Message:\n $message";
@@ -24,18 +24,19 @@
         if (!$_POST['message']) {
             $errMessage = 'Please enter your message';
         }
+        
         //Check if simple anti-bot test is correct
         if ($human !== 5) {
             $errHuman = 'Your anti-spam is incorrect';
         }
  
-// If there are no errors, send the email
-if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
-    if (mail ($to, $subject, $body, $from)) {
-        $result='<div class="alert alert-success">Thank You! I will be in touch</div>';
-    } else {
-        $result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
-    }
-}
+        // If there are no errors, send the email
+        if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
+            if (mail ($to, $subject, $body, $from)) {
+                $result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+            } else {
+                $result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later, or email me manually!</div>';
+            }
+        }
     }
 ?>
